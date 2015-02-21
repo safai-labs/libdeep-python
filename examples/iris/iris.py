32caafeaee7a857280d783a64b1187588ab98ec5
Bob Motram <bob@robotics.uk.to>
2015-02-21 19:48:43 +0000

    Iris classification example

---
+++ examples/iris/iris.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import sys
+import deeplearn
+
+# The number of iris species to classify
+no_of_classes = 3
+
+# Reads a number of data samples from a CSV file
+# where the expected output value is the fifth field (index 4)
+noOfSamples = deeplearn.readCsvFile("iris.data", 16, 3, [4], no_of_classes)
+print str(noOfSamples) + " samples loaded"
+
+# The error threshold (percent) for each layer of the network.
+# After going below the threshold the pre-training will move
+# on to the next layer
+deeplearn.setErrorThresholds([0.5, 0.5, 0.5, 1.0])
+
+# The learning rate in the range 0.0-1.0
+deeplearn.setLearningRate(0.1)
+
+# The percentage of dropouts in the range 0-100
+deeplearn.setDropoutsPercent(0.01)
+
+# Title of the training error image
+deeplearn.setPlotTitle("Iris Species Classification Training")
+
+# The number of time steps after which the training error image is redrawn
+deeplearn.setHistoryPlotInterval(500000)
+
+print "Training started"
+
+timeStep = 0
+while (deeplearn.training() != 0):
+    timeStep = timeStep + 1
+
+print "Training Completed"
+print "Test data set performance is " + str(deeplearn.getPerformance()) + "%";
+
+deeplearn.export("result.c")
+print "Exported trained network"
+
+deeplearn.save("result.nn")
+print "Saved trained network"
---
+++ examples/iris/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import deeplearn
+
+# Rather than showing numbers show the species names
+
+species = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+
+# Load the neural network
+
+deeplearn.load("result.nn")
+
+# These dimensions are similar to those which exist in the
+# data set, but are adjusted slightly so that the network
+# has never seen these exact values before
+
+print "Expected: " + species[0]
+deeplearn.test([5.44, 3.436, 1.667, 0.214])
+print "Returned: " + species[deeplearn.getClass()]
+
+print "\nExpected: " + species[1]
+deeplearn.test([6.14, 2.75, 4.04, 1.32])
+print "Returned: " + species[deeplearn.getClass()]
+
+print "\nExpected: " + species[2]
+deeplearn.test([6.71, 3.14, 5.92, 2.29])
+print "Returned: " + species[deeplearn.getClass()]
